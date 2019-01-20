a19bd71fe8bf73a912fb19cd5e1d793c13166461
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2019-01-20 01:23:28 +0100

    Chore: fix tests

--- tests/test_web.py
+++ tests/test_web.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 from aiohttp.test_utils import TestClient
 
 
-async def test_index(iftttie_client: TestClient):
+async def test_index_without_auth(iftttie_client: TestClient):
     response = await iftttie_client.get('/')
-    assert response.status == 200
+    assert response.status == 401
